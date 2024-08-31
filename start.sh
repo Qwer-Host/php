ce1dfadb4e9327faed234c726782d3713b75623b
CodeExplorerX <codeexplorerx@proton.me>
2024-08-31 03:35:30 +0200

    Graceful Shutdown

--- start.sh
+++ start.sh
@@ -2,8 +2,18 @@
 rm -rf /home/container/tmp/*
 
 echo "⟳ Starting PHP-FPM..."
-/usr/local/sbin/php-fpm --fpm-config /home/container/php-fpm/php-fpm.conf --daemonize
+/usr/local/sbin/php-fpm --fpm-config /home/container/php-fpm/php-fpm.conf
 
 echo "⟳ Starting Nginx..."
 echo "✓ Successfully started"
-/usr/sbin/nginx -c /home/container/nginx/nginx.conf -e stderr -p /home/container/ -g "daemon off;"
+/usr/sbin/nginx -c /home/container/nginx/nginx.conf -e stderr -p /home/container/ 
+
+while true; do
+  read -r line
+  if [ "${line}" = "stop" ]; then
+    /usr/sbin/nginx -s stop -c /home/container/nginx/nginx.conf -e stderr -p /home/container/
+    pkill php-fpm
+    rm -rf /home/container/tmp/*
+    exit 0
+  fi
+done
